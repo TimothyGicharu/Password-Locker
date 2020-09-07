504c20756f506a5cdb864e811136e891bfbb7d91
Timothy Munene <munenegicharu@gmail.com>
2020-09-07 12:26:18 +0300

    Remove commented code

--- run.py
+++ run.py
@@ -4,12 +4,15 @@ from login import User
 from login import Credentials
 
 # User Functions
+
+
 def create_user(user_name, user_password):
     """
     Function to create a new user
     """
     new_user = User(user_name, user_password)
     return new_user
+
 
 def save_user(user):
     """
@@ -25,12 +28,14 @@ def save_user(user):
 
 # Credentials Functions
 
-def create_credentials(account_name,user_name,password):
+
+def create_credentials(account_name, user_name, password):
     """
     Function to create user credentials
     """
-    new_credentials = Credentials(account_name,user_name,password)
+    new_credentials = Credentials(account_name, user_name, password)
     return new_credentials
+
 
 def save_credentials(credentials):
     """
@@ -38,17 +43,20 @@ def save_credentials(credentials):
     """
     credentials.save_credentials()
 
+
 def delete_credentials(credentials):
     """
     Function to delete a credentials
     """
     credentials.delete_credentials()
 
+
 def find_credentials(account_name):
     '''
     Function that finds a credential by number and returns the credential
     '''
     return Credentials.find_by_account_name(account_name)
+
 
 def display_credentials():
     """
@@ -57,6 +65,7 @@ def display_credentials():
     return Credentials.display_credentials()
 
 # Calling our Functions
+
 
 def main():
     print("Hello. Welcome to Password Locker. Create username...")
@@ -77,20 +86,19 @@ def main():
             print("What would you like to do?")
             print("\n")
             print("Use these short codes : cu - Create Username and password, dc - Display Credentials, sc - Search for credentials account, du - Delete Credentials, ex - Exit password locker")
-            # sc - Save Credentials, sc - Show / Display Credentials, dc - Delete credentials, ex - Exit User Account
             short_code = input().lower()
 
             if short_code == "cu":
                 print("~" * 15)
                 print("Enter account name.....")
-                account_name= input()
+                account_name = input()
                 print("Enter username ...")
                 user_name = input()
                 print("~" * 15)
                 print("Create password")
                 password = input()
-                
-                save_credentials(create_credentials(account_name,user_name,password))
+
+                save_credentials(create_credentials(account_name, user_name, password))
                 print("\n")
                 print("Account added")
                 print("Account: {account_name} User name: {user_name} Password: {password}")
@@ -102,7 +110,9 @@ def main():
                     print("\n")
 
                     for credentials in display_credentials():
-                        print("Account: {credentials.account_name} / Username: {credentials.user_name} / Password: {password}")
+                        print(credentials)
+                        print(
+                            "Account: {credentials.account_name} / Username: {credentials.user_name} / Password: {password}")
                     print("\n")
                 else:
                     print("No accounts saved.")
@@ -119,7 +129,7 @@ def main():
 
                 else:
                     print("That account does not exist")
-                
+
             elif short_code == "du":
                 print("Account to be deleted...")
                 delete_account = input()
@@ -139,62 +149,6 @@ def main():
     else:
         print("Wrong password!")
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-            #     #create and save a new user
-            #     save_user(create_user(user_name, password))
-            #     print("\n")
-            #     print("New user {} created.".format(user_name))
-
-            # elif short_code == "lu":
-            #     print("Enter user name.....")
-            #     user_name = input()
-
-            #     if user_name in User.users_list:
-            #         print("Logged in!")
-            #         print('\n')
-            #     else:
-            #         print("Failed")
-
-
-            # elif short_code == "du":
-            #     delete_account = input("Enter account name to be deleted...")
-            #     print("Are you sure you want to delete this {} account?".format(delete_account))
-
-            #     while True:
-            #         delete_account.delete_user()
-            #         break
-
-            # else:
-            #     print("Use the short codes provided!")
-
-
-            # # elif short_code == "lu":
-            # #     print("Enter user name .....")
-            # #     user_name = input()
-            # #     if user_name in User.users_list:
-            # #         print("You are good to go")
-            # #         print("Enter password.....")
-            # #         password = input()
-            # #         # if password ==
-            # #     else:
-            # #         print("User doesn't exist!")
-
-            #     # print("\n")
-            #     # print("Enter password .....")
-            #     # password = input()
-            #     # print("\n")
-            #     #
-            #     # for user in User.users_list:
-            #     #     print(user)
-            #     #     # if user_name ==
-
-
